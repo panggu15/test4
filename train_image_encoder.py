2fdfaeec8e843380e5494f7cfc3e17d231115d3a
mingoo <83053171+panggu15@users.noreply.github.com>
2024-03-24 08:02:54 +0900

    Update train_image_encoder.py

--- train_image_encoder.py
+++ train_image_encoder.py
@@ -766,7 +766,7 @@ def main(args):
                     "xFormers 0.0.16 cannot be used for training in some GPUs. If you observe problems during training, please update xFormers to at least 0.0.17. See https://huggingface.co/docs/diffusers/main/en/optimization/xformers for more details."
                 )
             unet.enable_xformers_memory_efficient_attention()
-            image_proj_model.enable_xformers_memory_efficient_attention()
+            # image_proj_model.enable_xformers_memory_efficient_attention()
         else:
             raise ValueError("xformers is not available. Make sure it is installed correctly")
 
