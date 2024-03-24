cfe16f0f3ab5a1bc5023c618acf71edbed6df4da
mingoo <83053171+panggu15@users.noreply.github.com>
2024-03-24 13:22:57 +0900

    Update train_image_encoder.py

--- train_image_encoder.py
+++ train_image_encoder.py
@@ -712,6 +712,26 @@ def main(args):
         args.pretrained_model_name_or_path, subfolder="unet", revision=args.revision
     )
 
+    class ImageProjModel(torch.nn.Module):
+        """Projection Model"""
+    
+        def __init__(self, cross_attention_dim=1024, clip_embeddings_dim=1024, clip_extra_context_tokens=4):
+            super().__init__()
+    
+            self.generator = None
+            self.cross_attention_dim = cross_attention_dim
+            self.clip_extra_context_tokens = clip_extra_context_tokens
+            self.proj = torch.nn.Linear(clip_embeddings_dim, self.clip_extra_context_tokens * cross_attention_dim)
+            self.norm = torch.nn.LayerNorm(cross_attention_dim)
+    
+        def forward(self, image_embeds):
+            embeds = image_embeds
+            clip_extra_context_tokens = self.proj(embeds).reshape(
+                -1, self.clip_extra_context_tokens, self.cross_attention_dim
+            )
+            clip_extra_context_tokens = self.norm(clip_extra_context_tokens)
+            return clip_extra_context_tokens
+    
     image_proj_model = ImageProjModel(
         cross_attention_dim=unet.config.cross_attention_dim,
         clip_embeddings_dim=image_encoder.config.projection_dim,
