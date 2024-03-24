b03d9ccb0695f49acc40abb5f8900476c47078bd
mingoo <83053171+panggu15@users.noreply.github.com>
2024-03-24 13:50:15 +0900

    Update train_image_encoder.py

--- train_image_encoder.py
+++ train_image_encoder.py
@@ -1045,7 +1045,7 @@ def main(args):
     if accelerator.is_main_process:
         image_proj_model = accelerator.unwrap_model(image_proj_model)
         torch.save(image_proj_model.state_dict(), os.path.join(args.output_dir, 'model.pt'))
-        image_proj_model.save_pretrained(args.output_dir)
+        # image_proj_model.save_pretrained(args.output_dir)
 
         if args.push_to_hub:
             save_model_card(
